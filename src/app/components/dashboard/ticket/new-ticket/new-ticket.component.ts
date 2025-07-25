6e941d94202b9ef790c6f1db85bef81618d2acd5
ibrahim hany <ihany941@gmail.com>
2025-07-25 16:39:09 +0300

    feat: implement form submission logic and enhance control component with input reference

--- src/app/components/dashboard/ticket/new-ticket/new-ticket.component.ts
+++ src/app/components/dashboard/ticket/new-ticket/new-ticket.component.ts
@@ -1,14 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, viewChild } from '@angular/core';
 import { ButtonComponent } from '../../../shared/button/button.component';
 import { ControlComponent } from "../../../shared/control/control.component";
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-ticket',
   standalone: true,
-  imports: [ButtonComponent, ControlComponent],
+  imports: [ButtonComponent, ControlComponent,FormsModule],
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.css'
 })
 export class NewTicketComponent {
+  @ViewChild("form")  form?:ElementRef<HTMLFormElement>;
+onSubmit(title:string,ticketText:string) {
+  console.log(title)
+  console.log(ticketText)
+  console.log(this.form)
+  this.form?.nativeElement.reset();
+}
 
 }
--- src/app/components/shared/control/control.component.ts
+++ src/app/components/shared/control/control.component.ts
@@ -1,4 +1,4 @@
-import { Component,ElementRef,HostBinding,HostListener,inject,Input, ViewEncapsulation } from '@angular/core';
+import { Component,ContentChild,ElementRef,HostBinding,HostListener,inject,Input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-control',
@@ -28,9 +28,13 @@ export class ControlComponent {
   // access host element
   private el=inject(ElementRef)
 
+  @ContentChild("input")private control?:ElementRef<HTMLInputElement|HTMLTextAreaElement>;
+
+  
   onClick(){
     console.log("clicked")
     console.log(this.el)
+    console.log(this.control)
   }
 
 }
